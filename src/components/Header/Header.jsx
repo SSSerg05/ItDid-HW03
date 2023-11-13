78a6608dba1b28336d683e79c5b2c16fb8f88cf7
SSSerg05 <ssserg05@gmail.com>
2023-11-13 14:06:55 +0200

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,15 @@ import { NavLink } from "react-router-dom";
 export const Header = () => {
 
   return (
-    <nav>
-      <ul>
-        <li><NavLink to="/">Главная</NavLink></li>
-        <li><NavLink to="/about">О сайте</NavLink></li>
-        <li><NavLink to="/category">Категории</NavLink></li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li><NavLink to="/">Главная</NavLink></li>
+          <li><NavLink to="/about">О сайте</NavLink></li>
+          <li><NavLink to="/category">Категории</NavLink></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
