01616ecd7772f138d9a5c4a45ded4fc9a8f6f8de
SSSerg05 <119066393+SSSerg05@users.noreply.github.com>
2023-11-13 09:10:31 +0000

    Deploying to gh-pages from @ SSSerg05/itgid-hw03@b78bd2c65bae7e76a9ae6deb333834c80435bd94 🚀

---
+++ static/js/544.4dd93f6d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkitgid_hw03=self.webpackChunkitgid_hw03||[]).push([[544],{544:function(e,n,t){t.r(n),t.d(n,{Home:function(){return r}});var i=t(184),r=function(){return(0,i.jsx)(i.Fragment,{children:(0,i.jsx)("h1",{children:"Home"})})};n.default=r}}]);
+//# sourceMappingURL=544.4dd93f6d.chunk.js.map
